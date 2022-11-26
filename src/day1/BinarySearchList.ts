49fa2771f0e017af5570af78340447543466fb42
anasmak04 <anasdev8@gmail.com>
2022-11-26 17:36:34 +0100

    implemented binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let firstIndex = 0;
+    let Lastindex = haystack.length;
 
+    while (firstIndex <= Lastindex) {
+        let middleIndex = Math.floor((firstIndex + Lastindex) / 2);
+        let v = haystack[middleIndex];
+
+        if (needle == v) {
+            return true;
+        } else if (needle < v) {
+            Lastindex = middleIndex - 1;
+        } else {
+            firstIndex = middleIndex + 1;
+        }
+    }
+
+    return false;
 }
